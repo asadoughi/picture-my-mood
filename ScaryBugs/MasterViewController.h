c079b00e399a482e9d542a015f326b945a428592
Amir Sadoughi <amir.sadoughi@gmail.com>
2013-11-09 19:32:48 -0600

    Initial Commit

---
+++ ScaryBugs/MasterViewController.h
@@ -0,0 +1,18 @@
+//
+//  MasterViewController.h
+//  ScaryBugs
+//
+//  Created by Amir Sadoughi on 11/9/13.
+//  Copyright (c) 2013 Nayeli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
